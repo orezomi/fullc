d17a5037f320eac59002ff0606a96f2e37d37c3c
orezomi <harismoaji@gmail.com>
2015-08-08 22:46:03 +0700

    Update FullcAsset.php

--- FullcAsset.php
+++ FullcAsset.php
@@ -8,7 +8,7 @@ class FullcAsset extends AssetBundle
 {
 	public $sourcePath = '@bower'; 
     public $css = [ 
-        'fullcalendar/dist/fullcalendar.min.css','fullcalendar/dist/fullcalendar.print.css'
+        'fullcalendar/dist/fullcalendar.min.css'
     ];
     public $js = [
     	'moment/min/moment.min.js','fullcalendar/dist/fullcalendar.min.js'
